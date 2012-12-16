8655c2055e8ae211230eca02a832fef58a431fe6
PointedEars <PointedEars@424ad142-79bb-4b33-8712-1f7f8a1e5664>
2012-12-16 04:01:37 +0000

    Fixed caching and character references

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
 <?php
 
+$encoding = 'UTF-8';
+header("Content-Type: text/html" . ($encoding ? "; charset=$encoding" : ""));
+
+$modi = max(array(
+  @filemtime(__FILE__),
+  @filemtime('index.xml'),
+  @filemtime('index.xsl'),
+  @filemtime('faq.css'),
+));
+
+header('Last-Modified: ' . gmdate('D, d M Y H:i:s', $modi) . ' GMT');
+
+/* Cached resource expires in HTTP/1.1 caches 0 (-24-) h after last retrieval */
+header('Cache-Control: max-age=0, s-maxage=0, must-revalidate, proxy-revalidate');
+
+/* Cached resource expires in HTTP/1.0 caches 0 (-24-) h after last retrieval */
+header('Expires: ' . gmdate('D, d M Y H:i:s', time() /*+ 86400*/) . ' GMT');
+
 $doc = new DOMDocument();
 $xsl = new XSLTProcessor();
 
